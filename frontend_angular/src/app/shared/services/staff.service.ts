5e64453a9334a223b6008f9e0a7889155a1cae00
Andrey <andreybogush202@gmail.com>
2018-08-19 15:01:48 -0700

    staff service table name

--- frontend_angular/src/app/shared/services/staff.service.ts
+++ frontend_angular/src/app/shared/services/staff.service.ts
@@ -41,15 +41,15 @@ export class StaffService {
   }
 
   getStaffList() {
-    return this.http.get(config.baseURL + 'api/users', { headers: this.header });
+    return this.http.get(config.baseURL + 'api/' + this.table_name, { headers: this.header });
   }
 
   addNewStaff(newStaff) {
-    return this.http.post(config.baseURL + 'api/users', newStaff, {headers: this.header});
+    return this.http.post(config.baseURL + 'api/' + this.table_name, newStaff, {headers: this.header});
   }
 
   editStaff(editStaff) {
-    return this.http.put(config.baseURL + 'api/users/' + editStaff.id, editStaff, {headers: this.header});
+    return this.http.put(config.baseURL + 'api/' + this.table_name + '/' + editStaff.id, editStaff, {headers: this.header});
   }
 
 }
