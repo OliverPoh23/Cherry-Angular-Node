c8dc43a1f3d76161b6714db7a337aab2a0498058
Andrey <andreybogush202@gmail.com>
2018-08-19 16:06:08 -0700

    manage action service

--- frontend_angular/src/app/shared/services/action.service.ts
+++ frontend_angular/src/app/shared/services/action.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Action } from '../modules/action.model';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { config } from '../modules/config.model';
 
 @Injectable()
 export class ActionService {
 
   actionList = [];
+  table_name = 'action';
+  token;
+  header;
 
-  constructor() {
-    
+  constructor(private http: HttpClient) {
+    this.token = localStorage.getItem('token');
+    this.header = new HttpHeaders({ 'token': this.token });
    }
 
   getActionList() {
