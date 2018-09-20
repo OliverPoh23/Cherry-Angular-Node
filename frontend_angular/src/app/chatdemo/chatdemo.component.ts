7ff390a1312d1219307b96f02ee86c2bf23c8604
Andrey <andreybogush202@gmail.com>
2018-09-20 10:31:54 -0700

    waiting list

--- frontend_angular/src/app/chatdemo/chatdemo.component.ts
+++ frontend_angular/src/app/chatdemo/chatdemo.component.ts
@@ -30,8 +30,6 @@ export class ChatdemoComponent implements OnInit {
     this.userId = activedRoute.snapshot.params['userId'];
     this.staffService.getStaffList().subscribe(staffs => {
       me.staffArray = staffs['data'];
-      console.log(me.staffArray);
-      
     });
 
     this.chatService.loadChatContent(this.staffId, this.userId).subscribe(chatContents => {
@@ -41,11 +39,6 @@ export class ChatdemoComponent implements OnInit {
           if (chatItem['message_type'] === 'userTostaff') {
             contactService.getUserProfile(chatItem['user_id']).subscribe(data => {
               if (data['error'] === 0) {
-                // me.userProfile = data['data'][0];
-                // username = data['data'][0]['first_name'] + data['data'][0]['first_name'];
-              
-                // me.profileArray.push(data['data'][0]);
-
                 var exist = false;
                 me.profileArray.map(temp => {
                   if (temp.id === data['data'][0]['id']) {
@@ -70,40 +63,11 @@ export class ChatdemoComponent implements OnInit {
       });
 
       var elmnt = document.getElementById('scrollToView');
-      // elmnt.scrollIntoView();
       setTimeout(() => {
         elmnt.scrollIntoView();
       }, 1000);
     });
-    // var contactData = {
-    //   status: 1
-    // };
-    // this.contactService.updateByUserId(this.userId, contactData).subscribe(data => {
-    //   if (data['success'] === 1) {
-    //     me.chatService.sendMsg({
-    //       type: 'changedstatus'
-    //     });
-    //   }
-    // });
-
-    // this.chatService.loadChatContent(this.staffId, this.userId).subscribe(chatContents => {
-
-    //   chatContents['data'].map(chatItem => {
-    //     me.chatContentsArray.push({
-    //       type: chatItem['message_type'],
-    //       staffId: me.staffId,
-    //       userId: me.userId,
-    //       msg: chatItem['message'],
-    //       isMedia: chatItem['isMedia'] === 0 ? false : true
-    //     });
-    //   });
-
-    //   var elmnt = document.getElementById('scrollToView');
-    //   // elmnt.scrollIntoView();
-    //   setTimeout(() => {
-    //     elmnt.scrollIntoView();
-    //   }, 1000);
-    // });
+    
   }
 
   ngOnInit() {
@@ -120,9 +84,12 @@ export class ChatdemoComponent implements OnInit {
 
       if (msg.text.type === 'startChat' && msg.text.userId.toString() === me.userId.toString()) {
         me.staffId =  msg.text.staffId;
-        // console.log(msg);
-        // console.log(me.staffId);
       }
+    });
+
+    this.chatService.sendMsg({
+      type : 'requestchat',
+      userId : this.userId
     });
   }
 
--- frontend_angular/src/app/dashboard/waitinglist/waitinglist.component.ts
+++ frontend_angular/src/app/dashboard/waitinglist/waitinglist.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ChatService } from '../../shared/services/chat.service';
+import { ContactsService } from '../../shared/services/contacts.service';
+import { config } from '../../shared/modules/config.model';
 
 @Component({
   selector: 'app-waitinglist',
@@ -6,10 +9,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./waitinglist.component.scss']
 })
 export class WaitinglistComponent implements OnInit {
-
-  constructor() { }
+  waitingList = [];
+  constructor(
+    private chatService: ChatService,
+    private contactsService: ContactsService
+  ) { }
 
   ngOnInit() {
+    var me = this;
+    this.chatService.messages.subscribe(msg => {
+      if (msg.text.type === 'requestchat') {
+        var userId = msg.text.userId;
+        var isExist = false;
+        me.waitingList.map(waitItem => {
+          if (userId === waitItem.userId) {
+            isExist = true;
+          }
+        });
+        
+        if (isExist) {
+          return;
+        }
+
+        me.contactsService.getUserProfile(userId).subscribe(user => {
+          if (user['error'] === 0 && user['data'].length > 0) {
+            // contact['profile_image'] = user['data'][0]['image'];
+            if (user['data'][0]['image'] === '' || user['data'][0]['image'] === '/upload/profile-placeholder.png') {
+              user['data'][0]['image'] = config.baseURL + 'avartar.png';
+            }
+            me.waitingList.push({
+              userId: userId,
+              image: user['data'][0]['image'],
+              name: user['data'][0]['first_name'] + ' ' + user['data'][0]['last_name']
+            });
+          }
+        });
+        
+      }
+    });
   }
 
 }
